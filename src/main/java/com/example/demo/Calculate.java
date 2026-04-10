a871f3aa6a1b9ed756175c43abdaa4022558b3b4
Takudzwa Jauki <121710070+Tjauking@users.noreply.github.com>
2026-04-10 11:20:23 +0530

    init

--- src/main/java/com/example/demo/Calculate.java
+++ src/main/java/com/example/demo/Calculate.java
@@ -1,5 +1,32 @@
 package com.example.demo;
 
-public class Calculate {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+@Controller
+public class Calculate {
+	@GetMapping("/")
+	public String showForm() {
+		return "index";
+	}
+	
+	@PostMapping("/calculate")
+	public String calculateTable(@RequestParam("num") int num,Model model) {
+		int [] result=new int[10];
+		for(int i=1;i<10;i++) {
+			result[i-1]=num*i;
+			
+		}
+		model.addAttribute("result",result);
+		model.addAttribute("num",num);
+		return "result";
+	}
+	
+	
+	
+
+	
 }
